7c1d441cbaeb8f9ecb45f55d8c5b02410521331f
Siguma <you8jun5@gmail.com>
2017-02-15 13:22:48 +0900

    スプライトの追加と追加時に形式を変える機能追加

---
+++ Assets/Editor/TextureImporterSettting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace editor {
+    public sealed class TextureImporterSettting : AssetPostprocessor {
+
+        public override int GetPostprocessOrder() {
+            return 0;
+        }
+
+        private void OnPreprocessTexture() {
+            TextureImporter texture_importer = assetImporter as TextureImporter;
+
+            texture_importer.filterMode = FilterMode.Point;
+        }
+    }
+}
