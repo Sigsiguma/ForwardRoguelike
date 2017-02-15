b9564811cc020d1033e3c6fcfdcb0b3611aaf93a
Siguma <you8jun5@gmail.com>
2017-02-15 19:54:40 +0900

    DOTweenの追加

---
+++ Assets/Scripts/InGame/Camera/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace ingame.camera {
+    public class FollowCamera : MonoBehaviour {
+
+        [SerializeField]
+        private Transform target_;
+
+        private void Start() {
+            this.LateUpdateAsObservable()
+                .Subscribe(_ => {
+                    var pos = transform.position;
+                    pos.y = target_.position.y + 1.0f;
+                    transform.position = pos;
+                });
+        }
+    }
+}
