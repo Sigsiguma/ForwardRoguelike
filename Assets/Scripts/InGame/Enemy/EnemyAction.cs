603de4c81b95f105f8641f6168201900859e9740
Siguma <you8jun5@gmail.com>
2017-02-19 14:30:08 +0900

    敵の行動を呼び出すActionクラスを追加

---
+++ Assets/Scripts/InGame/Enemy/EnemyAction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ingame.enemy {
+    [RequireComponent(typeof(EnemyMove))]
+    [RequireComponent(typeof(EnemyAttack))]
+    public class EnemyAction : MonoBehaviour {
+
+        private EnemyMove move_;
+        private EnemyAttack attack_;
+
+        private void Awake() {
+            move_ = GetComponent<EnemyMove>();
+            attack_ = GetComponent<EnemyAttack>();
+        }
+
+        public void Action(ActionType action) {
+            switch (action) {
+                case ActionType.Move:
+                    move_.Move();
+                    break;
+                case ActionType.Attack:
+                    attack_.Attack();
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+
+    public enum ActionType {
+        None,
+        Move,
+        Attack
+    };
+}
---
+++ Assets/Scripts/InGame/Enemy/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ingame.enemy {
+    public class EnemyManager : MonoBehaviour {
+
+        private List<EnemyAction> enemy_list_;
+        private List<EnemyAction> attack_list_;
+        private List<EnemyAction> move_list_;
+
+        private void Awake() {
+            enemy_list_ = new List<EnemyAction>();
+            attack_list_ = new List<EnemyAction>();
+            move_list_ = new List<EnemyAction>();
+        }
+
+        private void EnemysAction() {
+
+        }
+
+        public void AddList(EnemyAction action) {
+
+            if (action == null) return;
+
+            enemy_list_.Add(action);
+        }
+    }
+}
